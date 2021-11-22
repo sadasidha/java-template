83dca00762ad24f7db06cb5672e78cad506b5457
Mohibur Rashid <johny10151981@yahoo.com>
2021-11-22 09:42:37 +0900

    Remove Guava

---
+++ src/main/java/simple/mind/template/Strings.java
@@ -0,0 +1,7 @@
+package simple.mind.template;
+
+class Strings {
+  static boolean isNullOrEmpty(String string) {
+    return string == null || string.isEmpty();
+  }
+}
--- src/main/java/simple/mind/template/TemplateProcessor.java
+++ src/main/java/simple/mind/template/TemplateProcessor.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.base.Strings;
+//import com.google.common.base.Strings;
 
 /**
  * @author Mohibur Rashid
